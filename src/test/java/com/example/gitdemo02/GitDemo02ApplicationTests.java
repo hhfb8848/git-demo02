eda7d248673bb9092d5860abb0a7647165d573fe
hhfb8848 <sjh6690661@163.com>
2023-06-16 10:21:43 +0800

    Initial commit

---
+++ src/test/java/com/example/gitdemo02/GitDemo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitdemo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemo02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
