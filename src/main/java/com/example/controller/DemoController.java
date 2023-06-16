9b8fa21254b973a2f8238b57905c4936f195a93d
hhfb8848 <sjh6690661@163.com>
2023-06-16 11:16:15 +0800

    第一个方法

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,10 +1,21 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author sjh
  * @version 1.0
  * @date 2023/6/16 11:02
  * @description: TODO
  */
+@RestController
+@RequestMapping("/demo")
 public class DemoController {
+
+    @GetMapping("/1")
+    public String first(){
+        return "第一个版本";
+    }
 }
--- src/main/java/com/example/gitdemo02/GitDemo02Application.java
+++ src/main/java/com/example/gitdemo02/GitDemo02Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitDemo02Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(GitDemo02Application.class, args);
     }
 
